f8be54e02707593926a4bc581cfc6c90ee4a6f54
andrewPoulton <awdpoulton@gmail.com>
2020-12-04 13:28:09 +0400

    2020 day4

--- aoc_2020/src/lib.rs
+++ aoc_2020/src/lib.rs
@@ -4,6 +4,8 @@ mod day2;
 pub use crate::day2::day2;
 mod day3;
 pub use crate::day3::day3;
+mod day4;
+pub use crate::day4::day4;
 mod utils;
 pub use crate::utils::*;
 
--- aoc_2020/src/main.rs
+++ aoc_2020/src/main.rs
@@ -1,9 +1,9 @@
 use std::env;
 use aoc_2020::*;
 
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    day3(&args[1]);
+    day4(&args[1]);
 }
-
