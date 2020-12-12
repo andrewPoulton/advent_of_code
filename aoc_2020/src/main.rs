3639c552034b71409b6911993ecd59a6ae4c6502
andrewPoulton <awdpoulton@gmail.com>
2020-12-12 12:55:34 +0400

    match fn run by input day

--- aoc_2020/src/main.rs
+++ aoc_2020/src/main.rs
@@ -6,7 +6,24 @@ use std::time::{Instant};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let start = Instant::now();
-    day11(&args[1]);
+    let fname = format!("src/day{}.txt", args[1]);
+    match &args[1][..]{
+        "1" => day1(&fname),
+        "2" => day2(&fname),
+        "3" => day3(&fname),
+        "4" => day4(&fname),
+        "5" => day5(&fname),
+        "6" => day6(&fname),
+        "7" => day7(&fname),
+        "8" => day8(&fname),
+        "9" => day9(&fname),
+        "10" => day10(&fname),
+        "11" => day11(&fname),
+        "12" => day12(&fname),
+        _ => {
+            println!("Day {} is not a thing yet, or ever", args[1]);
+        }
+    };
     let duration = start.elapsed();
     println!("overall it took {:?}", duration);
 }
