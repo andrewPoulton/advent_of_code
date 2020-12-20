eb9fd9b62c39db00de66438a39385078d76b76b5
andrewPoulton <awdpoulton@gmail.com>
2020-12-20 17:59:20 +0000

    day17 part 1, download script

--- aoc_2020/src/day17.rs
+++ aoc_2020/src/day17.rs
@@ -7,117 +7,124 @@ pub fn day17(filename: &String){
 
     // let mut z_index: usize = 0; //keep track of  
     let mut pocket_dimension:VecDeque<ZPlane> = VecDeque::from(vec![ZPlane::init(&contents)]);
+    println!("intitial:\n{}", pocket_dimension[0]);
+    let x = pocket_dimension[0].h + 20;
+    pocket_dimension[0].expand_to(x);
+    for _ in 0..8{
+        pocket_dimension.push_front(ZPlane::blank(x));
+        pocket_dimension.push_back(ZPlane::blank(x));
+    }
+
+    let mut part2_dimensions = VecDeque::from(vec![pocket_dimension.clone()]);
+    for _ in 0..8{
+        let mut front_dim = VecDeque::new();
+        let mut back_dim = VecDeque::new();
+        for _ in 0..pocket_dimension.len(){
+            front_dim.push_back(ZPlane::blank(x));
+            back_dim.push_back(ZPlane::blank(x));
+        }
+        part2_dimensions.push_back(back_dim);
+        part2_dimensions.push_front(front_dim);
+    }
 
 
-    println!("{:?}", pocket_dimension);
+    // println!("{}", pocket_dimension[1]);
+    // println!("{}")
     // println!("{:?}", pocket_dimension[0].cubes[2][5]);
-    println!("{}-{}", pocket_dimension[0].h, pocket_dimension[0].w);
-    println!("{}", pocket_dimension[0].active_neighours(-1, 0, false));
-
+    // println!("{}-{}", pocket_dimension[0].h, pocket_dimension[0].w);
+    // println!("{}", pocket_dimension[0].active_neighours(-1, 0, false));
+    
     for _ in 0..6{
         pocket_dimension = tick(&pocket_dimension);
     }
+    for pocket in &pocket_dimension{
+        println!("{}", pocket);
+    }
     let part_1_ans = pocket_dimension.iter().map(|plane| plane.count_active()).sum::<i32>();
     println!("{}", part_1_ans);
+    // let mut i = -1;
+    // for dim in &pocket_dimension{
+    //     println!("Z = {}", i);
+    //     println!("{}\n\n", dim);
+    //     i +=1 ;
+    // }
 }
 
 fn tick(pocket: &VecDeque<ZPlane>)->VecDeque<ZPlane>{
-    let mut ptr: i32 = -1;
-    let mut new_pocket: VecDeque<ZPlane> = VecDeque::new();
-    while ptr <= pocket.len() as i32{
-        let mut y = 0;
-        let mut x = 0;
-        let plane = if (ptr < pocket.len() as i32) & (ptr >=0) {
-            &pocket[ptr as usize]
-        } else if ptr >= 0{
-            &pocket[ptr as usize-1]
-        } else {
-            &pocket[0]
-        };
-        let mut new_plane = plane.clone();
-        new_plane.h += 1;
-        new_plane.w += 1;
-        
-        // iterate through existing rows, appending to end
-        while y < plane.h as i32{
-            while x <= plane.w as i32 {
-                let new_cube = neighbour_count(pocket, x, y, ptr );
-                if x == plane.w as i32{
-                    new_plane.cubes[y as usize].push_back(new_cube)
-                } else{
-                    new_plane.cubes[y as usize][x as usize] = new_cube
-                }
-                x+=1;
+    let mut i: usize = 1;
+    let mut out_plane = VecDeque::new();
+    out_plane.push_back(pocket[0].clone());
+    while i< pocket.len()-1{
+        let prev_plane = &pocket[i-1];
+        let plane = &pocket[i];
+        let next_plane = &pocket[i+1];
+        let mut new_plane = pocket[i].clone();
+        for i in 1..new_plane.cubes.len()-1{
+            for j in 1..new_plane.cubes[0].len()-1{
+                let mut neighbours: u8 = 0;
+                neighbours += plane.active_neighours(i as i32, j as i32, false);
+                neighbours += prev_plane.active_neighours(i as i32, j as i32, true);
+                neighbours += next_plane.active_neighours(i as i32, j as i32, true);
+                new_plane.cubes[i][j] = State::from_count(&plane.cubes[i][j], neighbours);
             }
-            y+=1;
         }
-        x = 0;
-        y = plane.h as i32;
-        //add new last row
-        let mut last_row = VecDeque::new();
-        while x< plane.w as i32 {
-            let new_cube = neighbour_count(pocket, x, y, ptr);
-                last_row.push_back(new_cube);
-            x+=1;
-        }
-        new_plane.cubes.push_back(last_row);
-
-        // add new first row
-        y = -1;
-        x = 0;
-        let mut first_row = VecDeque::new();
-        while x< plane.w as i32 {
-            let new_cube = neighbour_count(pocket, x, y, ptr);
-            first_row.push_back(new_cube);
-            x+=1;
-        }
-        new_plane.cubes.push_front(first_row);
-
-        //add new first col
-        for row in &mut new_plane.cubes {
-            let new_cube = neighbour_count(pocket, x, y, ptr);
-            row.push_front(new_cube);
-        }
-        new_pocket.push_back(new_plane);
-        ptr += 1;
+        out_plane.push_back(new_plane);
+        i+=1;
     }
-    new_pocket
+    out_plane.push_back(pocket[pocket.len()-1].clone());
+    out_plane
+    // *pocket
 }
-
-fn neighbour_count(pocket: &VecDeque<ZPlane>, x:i32, y:i32, z:i32)->State{
-    let include_self = (z < pocket.len() as i32) & (z>=0);
-    let plane = if include_self {
-        &pocket[z as usize]
-    } else if z == -1{
-        &pocket[0]
-    } else {
-        &pocket[pocket.len()-1]
-    };
-    let h = plane.h as i32;
-    let w = plane.w as i32;
-    let mut neighbours = 0;
-    neighbours += plane.active_neighours(y,x, include_self);
-    if z > 0 {
-        neighbours += pocket[z as usize -1].active_neighours(y,x, true);
-    }
-    if z < pocket.len() as i32 -1 {
-        // println!("z is {}", z);
-        neighbours += pocket[if z==-1{0}else{z as usize +1}].active_neighours(y,x, true);
-    }
-    let curr_state = if (x<=0) | (x>= plane.w as i32) | (y<=0) | (y>=plane.h as i32){
-        &State::Inactive
-    } else {
-        // println!("{}-{}-{}", y,x,z);
-        &plane.cubes[y as usize][x as usize]
-    };
-    State::from_count(curr_state, neighbours)
-}
+// fn tick_part2(pocket: &VecDeque<VecDeque<ZPlane>>)->VecDeque<VecDeque<ZPlane>>{
+//     let mut i: usize = 1;
+//     let mut out_plane = VecDeque::from(vec![VecDeque::new()]);
+//     out_plane.push_back(pocket[0].clone());
+//     while i< pocket.len()-1{
+//         let prev_plane = &pocket[i-1];
+//         let plane = &pocket[i];
+//         let next_plane = &pocket[i+1];
+//         let mut new_plane = pocket[i].clone();
+//         for i in 1..new_plane.cubes.len()-1{
+//             for j in 1..new_plane.cubes[0].len()-1{
+//                 let mut neighbours: u8 = 0;
+//                 neighbours += plane.active_neighours(i as i32, j as i32, false);
+//                 neighbours += prev_plane.active_neighours(i as i32, j as i32, true);
+//                 neighbours += next_plane.active_neighours(i as i32, j as i32, true);
+//                 new_plane.cubes[i][j] = State::from_count(&plane.cubes[i][j], neighbours);
+//             }
+//         }
+//         out_plane.push_back(new_plane);
+//         i+=1;
+//     }
+//     out_plane.push_back(pocket[pocket.len()-1].clone());
+//     out_plane
+//     // *pocket
+// }
+// fn neighbour_count(pocket: &VecDeque<ZPlane>, x:i32, y:i32, z:i32)->State{
+//     todo!();
+// }
 
 #[derive(Debug, Clone)]
 struct ZPlane{
     h: usize,
     w: usize,
     cubes: VecDeque<VecDeque<State>>
+}
+
+use std::fmt::Display;
+impl Display for ZPlane {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for row in &self.cubes{
+            for cube in row{
+                match cube {
+                    State::Active => write!(f, "#")?,
+                    State::Inactive => write!(f, ".")?
+                }
+            }
+            write!(f,"\n")?;
+        }
+        write!(f, "\n")
+    }
 }
 
 
@@ -144,6 +151,34 @@ impl ZPlane {
         ZPlane { h: cubes.len(), w: cubes[0].len(), cubes: cubes}
     }
 
+    fn expand_to(&mut self, side_length: usize){
+        let diff = (side_length - self.cubes.len()) / 2;
+        for row in self.cubes.iter_mut(){
+            for _ in 0..diff{
+                row.push_front(State::Inactive);
+                row.push_back(State::Inactive);
+            }
+        }
+        for _ in 0..diff{
+            self.cubes.push_front(VecDeque::from(vec![State::Inactive; self.cubes[0].len()]));
+            self.cubes.push_back(VecDeque::from(vec![State::Inactive; self.cubes[0].len()]));
+        }
+        self.w = self.cubes[0].len();
+        self.h = self.cubes.len();
+    }
+
+    fn blank(side_length: usize)->ZPlane{
+        let mut cubes = VecDeque::new();
+        for i in 0..side_length{
+            let mut row = VecDeque::new();
+            for j in 0..side_length{
+                row.push_back(State::Inactive)
+            }
+            cubes.push_back(row);
+        }
+        ZPlane{ h: side_length, w:side_length, cubes}
+    }
+
     fn active_neighours(&self, i: i32, j:i32, and_self: bool)-> u8{
         let mut count:u8 = 0;
         let h = self.h as i32;
@@ -156,7 +191,7 @@ impl ZPlane {
                     let x = j+ *dx;
                     let y = i+ *dy;
                     if (0<=x) & (x<w) & (0<=y) & (y<h) {
-                        println!("{}-{}-{}-{}", y, x, w, h);
+                        // println!("{}-{}-{}-{}", y, x, w, h);
                         count += self.cubes[y as usize][x as usize] as u8;
                     }
                 }
--- aoc_2020/src/main.rs
+++ aoc_2020/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use aoc_2020::*;
+use fmt::write;
 
 use std::time::{Instant};
 
@@ -28,9 +29,10 @@ fn main() {
         "17" => day17(&fname),
         "18" => day18(&fname),
         "19" => day19(&fname),
-        "test.txt" => day19(&"test.txt".to_string()),
+        "20" => day20(&fname),
+        "test.txt" => day17(&"test.txt".to_string()),
         _ => {
-            println!("Day {} is not a potential_fields yet, or ever", args[1]);
+            println!("Day {} is not a potential day yet, or ever", args[1]);
         }
     };
     let duration = start.elapsed();
@@ -64,4 +66,168 @@ pub fn day19(filename: &String){
     println!("{:?}", rules);
     println!("{:?}", messages);
 
+}
+
+
+pub fn day20(filename: &String){
+    let contents = file2vec::<String>(filename);
+    let contents:Vec<String> =  contents.iter().map(|x| x.to_owned().unwrap()).collect();
+    let mut tiles = Vec::new();
+    let mut tile:Vec<Vec<char>> = Vec::new();
+    let mut id = String::from("");
+    for line in &contents{
+        if line.contains("Tile "){
+            for ch in line.chars(){
+                if ch.is_numeric(){
+                    id.push(ch);
+                };
+            };
+        } else if line.contains('#') | line.contains('.') {
+            tile.push(line.to_owned().chars().collect());
+        }
+        else if id.len()>0{
+            let new_tile = tile.drain(..).collect();
+            
+            tiles.push(Tile::new(id.parse::<i32>().unwrap(), new_tile));
+            id = String::from("");
+        }
+    }
+    
+    let mut tile = Box::new(tiles[0].clone());
+    let tile2 =  tiles[1].clone();
+
+    tile.north = Some(Box::new(tile2));
+    println!("{}", tile);
+    tiles.swap_remove(0);
+    // println!("{:?}", tile.north.unwrap().bottom_boarder());
+    // let boarders = vec![&tile.bottom_boarder(), &tile.top_boarder(), &tile.left_boarder(), &tile.right_boarder()];
+    let i = 0;
+    while i < tiles.len(){
+        
+    }
+}
+#[derive(Debug, Clone)]
+struct Tile{
+    id: i32,
+    tile: Vec<Vec<char>>,
+    north: Option<Box<Tile>>,
+    south: Option<Box<Tile>>,
+    east: Option<Box<Tile>>,
+    west: Option<Box<Tile>>,
+}
+
+use std::fmt;
+impl fmt::Display for Tile {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f,"{}\n", self.id)?;
+        for row in &self.tile{
+            for ch in row {
+                write!(f, "{} ", ch)?;
+            }
+            write!(f, "\n")?;
+        };
+        match &self.north {
+            Some(x) => write!(f,"North:\n{}", x),
+            None => write!(f, "")
+        }
+    }
+}
+
+impl Tile{
+    fn new(id:i32, tile:Vec<Vec<char>>)->Self {
+        Self { id, tile, north:None, south:None, east: None, west:None }
+    }
+    fn rotate(&mut self){
+        //transpose then reverse
+        let mut new_tiles = self.tile.clone();
+        let cols = self.tile[0].len();
+        for i in 0..self.tile.len(){
+            for j in 0..cols{
+                new_tiles[j][cols-i-1] = self.tile[i][j]
+            }
+        }
+        self.tile = new_tiles;
+    }
+
+    fn flip_horizontally(&mut self){
+        let mut new_tiles = self.tile.clone();
+        let cols = self.tile[0].len();
+        for i in 0..self.tile.len(){
+            for j in 0..cols{
+                new_tiles[i][cols-j-1] = self.tile[i][j]
+            }
+        }
+        self.tile = new_tiles;
+    }
+
+    fn flip_vertically(&mut self){
+        let mut new_tiles = self.tile.clone();
+        let rows = self.tile.len();
+        for i in 0..self.tile.len(){
+            for j in 0..rows{
+                new_tiles[rows-i-1][j] = self.tile[i][j]
+            }
+        }
+        self.tile = new_tiles;
+    }
+
+    fn rotate_n(&mut self, n: usize){
+        for _ in 0..n{
+            self.rotate();
+        }
+    }
+
+    fn top_boarder(&self)->Vec<usize>{
+        self.tile
+        .first()
+        .unwrap()
+        .iter()
+        .enumerate()
+        .filter_map(|(i,ch)|{
+            if ch == &'#'{
+                Some(i)
+            }else{
+                None
+            }
+        }).collect()
+    }
+
+    fn bottom_boarder(&self)->Vec<usize>{
+        self.tile
+        .last()
+        .unwrap()
+        .iter()
+        .enumerate()
+        .filter_map(|(i,ch)|{
+            if ch == &'#'{
+                Some(i)
+            }else{
+                None
+            }
+        }).collect()
+    }
+
+    fn right_boarder(&self)->Vec<usize>{
+        let mut boarder = Vec::new();
+        for i in 0..self.tile.len(){
+            if self.tile[i].last().unwrap() == &'#'{
+                boarder.push(i);
+            }
+        }
+        boarder
+    }
+
+    fn left_boarder(&self)->Vec<usize>{
+        let mut boarder = Vec::new();
+        for i in 0..self.tile.len(){
+            if self.tile[i].first().unwrap() == &'#'{
+                boarder.push(i);
+            }
+        }
+        boarder
+    }
+
+    // fn in_top_row(&mut self, other: &Vec<Tile>)->bool{
+
+    // }
 }
